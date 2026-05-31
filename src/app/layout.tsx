c66e7b887fabea06f8492435d6000dcf0e105261
martedgeorge <martedgeorge@gmail.com>
2026-05-30 20:05:33 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "George Marte Portfolio",
+  description: "Graphic Designer and Aspiring Art Director",
+};
 
 export default function RootLayout({
   children,
